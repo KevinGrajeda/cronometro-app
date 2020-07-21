2e9677a2b413d8e795a47b12469e6130d1a27a27
KevinGrajeda <yokevin20@gmail.com>
2020-07-20 23:57:59 -0500

    Updates

---
+++ precache-manifest.72dab8a035539c974e17d4688e12fbcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa30d0824480a799f0229a1b2c743592",
+    "url": "/cronometro-app/index.html"
+  },
+  {
+    "revision": "8b1fa910bcacf1c1fa1d",
+    "url": "/cronometro-app/static/css/main.295444fe.chunk.css"
+  },
+  {
+    "revision": "1025dc9566ec82965999",
+    "url": "/cronometro-app/static/js/2.a6973dbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cronometro-app/static/js/2.a6973dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b1fa910bcacf1c1fa1d",
+    "url": "/cronometro-app/static/js/main.18ba9059.chunk.js"
+  },
+  {
+    "revision": "1cf99078e6618940350b",
+    "url": "/cronometro-app/static/js/runtime-main.daee897c.js"
+  }
+]);
